5bdbb2ce15e0001c76ea53647f571681f88c179c
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-01-11 09:02:15 +0900

    Fix broken syntax for doc.go

--- mirakurun/doc.go
+++ mirakurun/doc.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by MIT
 // license that can be found in the LICENSE file.
 
-+/*
-+go-mirakurun is a Mirakurun Client for Go.
-+
-+*/
-+package mirakurun
+/*
+go-mirakurun is a Mirakurun Client for Go.
+
+*/
+package mirakurun
